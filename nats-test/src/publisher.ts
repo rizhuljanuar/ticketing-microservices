0aab89954135cc9074eb082ce922cb8845a52ecf
Rizhul Januar <rayjam66@gmail.com>
2024-06-06 09:28:32 +0700

    feat: connecting to NATS in a Nodejs

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatePublisher } from './events/ticket-create-publisher';
 
 console.clear();
 
@@ -6,16 +7,28 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 20000
-  });
+  const publisher = new TicketCreatePublisher(stan);
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20000
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: 20000
+  // });
+  //
+  // stan.publish('ticket:created', data, () => {
+  //   console.log('Event published');
+  // });
 });
