45c1203d485a600b406d502de83749400ac451ca
Rizhul Januar <rayjam66@gmail.com>
2024-06-10 16:35:30 +0700

    feat: cross-service data repplication in action

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+it('return an error if the ticket does not exist', async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20000,
+  });
+
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: 'asdasd',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it ('reserved a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20000,
+  });
+
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
+
+it.todo('emits an order creasted event');
---
+++ orders/src/routes/delete.ts
@@ -0,0 +1,28 @@
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@rjrtickets/common';
+import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/order';
+
+const router = express.Router();
+
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  // publish an event saying order was cancelled
+
+  res.status(204).send(order);
+});
+
+export { router as deleteOrderRouter };
